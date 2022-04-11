5fd11f88fac74ea27f102a1b2ac7ae706139dd77
John Woffindin <j.woffindin@gmail.com>
2022-04-12 08:47:19 +1200

    [feat] Basic 'var' interpolation

--- stk/config.py
+++ stk/config.py
@@ -1,39 +1,117 @@
 
 from os import path
+from sys import exc_info
 from yaml import safe_load
+from jinja2 import Template, Environment, StrictUndefined
+from dataclasses import dataclass
+
 class ConfigFile(dict):
-  EXPECTED_KEYS = ['vars', 'params', 'environments']
-  def __init__(self, filename: str):
-    self.filename = filename
+    EXPECTED_KEYS = ['vars', 'params', 'environments']
+    def __init__(self, filename: str):
+        self.filename = filename
 
-    self['vars'] = {}
-    self['params'] = {}
+        self['vars'] = {}
+        self['params'] = {}
 
-    cfg = safe_load(open(filename)) or dict()
-    super().__init__(cfg)
-    self._ensure_valid_keys()
+        cfg = safe_load(open(filename)) or dict()
+        super().__init__(cfg)
+        self._ensure_valid_keys()
 
-  def _ensure_valid_keys(self):
-    """
-    Ensure config file only contains expected keys
-    """
-    unknown_keys = set(self.keys()) - set(self.EXPECTED_KEYS)
+    def _ensure_valid_keys(self):
+        """
+        Ensure config file only contains expected keys
+        """
+        unknown_keys = set(self.keys()) - set(self.EXPECTED_KEYS)
 
-    if unknown_keys:
-      raise Exception(f"Config file {self.filename} has unexpected keys: {unknown_keys}")
-
-
+        if unknown_keys:
+            raise Exception(f"Config file {self.filename} has unexpected keys: {unknown_keys}")
 
 class Config:
-  def __init__(self, name: str, environment: str, config_path: str, var_overrides: dict = {}, param_overrides: dict = {}):
-    self.name = name
-    self.environment = environment
-    self.config_path = config_path
+    @dataclass
+    class InterpolationError:
+        key: str
+        value: str
+        error: str
+    class Vars(dict):
+        MAX_INTERPOLATION_DEPTH = 10
 
-    self.cfg = ConfigFile(path.join(config_path, name + '.yml'))
+        def __init__(self, vars: dict):
+            """
+            We want to expand interpolated 'vars' using Jinja2.
 
-  def vars(self):
-    return self.cfg['vars']
+            This is a bit different than how to expand other Jinja-value dicts because we'll need to iterate through a few
+            times until all vars have been expanded.
+            """
+            failed_keys = self.expand(vars)
 
-  def params(self):
-    return self.cfg['params']
+            if failed_keys:
+                raise Exception(f"An error occurred: {failed_keys}")
+
+            print(f"final {self}")
+
+        def expand(self, vars: dict):
+            """
+            Updates `self` with final interpolated values from initial dict `vars`.
+
+            Returns dict InterpolationError for all keys that can't be expanded
+            """
+            env = Environment(undefined=StrictUndefined)
+
+            interpolation_depth = 0
+
+            errors = {}
+            while interpolation_depth <= self.MAX_INTERPOLATION_DEPTH:
+                interpolation_depth += 1
+                unexpanded_keys = sorted(set(vars.keys()) - set(self.keys()))
+                print(f'unexpanded = {unexpanded_keys}')
+
+                if not unexpanded_keys:
+                    return None
+
+                for key in unexpanded_keys:
+                    print("Processing " + key)
+                    value = vars[key]
+                    print(value)
+                    try:
+                        if type(value) in [bool, dict, list, str]:
+                            tpl = env.from_string(str(value)) # convert value to jinja2 template
+                            result = str(tpl.render(self))
+
+                            value = safe_load(result) or ""
+                            self[key] = value
+                        else:
+                            # Don't try and process this value as Jinja template
+                            self[key] = value
+                        del errors[key]
+                        del vars[key]
+                    except Exception:
+                        errors[key] = Config.InterpolationError(key, value, exc_info()[1])
+
+            return errors
+
+
+
+
+    def __init__(self, name: str, environment: str, config_path: str, var_overrides: dict = {}, param_overrides: dict = {}):
+        self.name = name
+        self.environment = environment
+        self.config_path = config_path
+
+        self._cfg = ConfigFile(path.join(config_path, name + '.yml'))
+        self._vars = Config.Vars(self._cfg['vars'])
+
+
+    def vars(self):
+        return self._vars
+
+    def var(self, name):
+        return self._vars.get(name)
+
+    def params(self):
+        return self._cfg['params']
+
+    def param(self, name):
+        pp = self.params()
+        if name not in pp:
+            return None
+        return pp[name]
--- stk/tests/test_config.py
+++ stk/tests/test_config.py
@@ -4,18 +4,27 @@ import pytest
 from os import path
 from ..config import Config
 
-class TestConfig():
-  def fixture_path(self, *dir):
-    return path.join(path.dirname(__file__), "fixtures", *dir)
+class ConfigFixtures():
+    def fixture_path(self, *dir):
+        return path.join(path.dirname(__file__), "fixtures", *dir)
 
-  def test_empty(self):
-    c = Config(name="empty", environment="foo", config_path=self.fixture_path('config'))
-    assert list(c.vars().keys()) == []
-    assert list(c.params().keys()) == []
+    @pytest.fixture
+    def config(self, request):
+        return Config('main', environment="test", config_path=self.fixture_path('config', request.param))
 
-  def test_load(self):
-    c = Config(name="simple", environment="foo", config_path=self.fixture_path('config'))
 
-    assert list(c.vars().keys()) == ['foo']
-    assert list(c.params().keys()) == ['jane']
+@pytest.mark.parametrize('config', ['empty'], indirect=True)
+class TestEmptyConfig(ConfigFixtures):
+    def test_empty(self, config):
+        assert list(config.vars().keys()) == []
+        assert list(config.params().keys()) == []
 
+@pytest.mark.parametrize('config', ['simple'], indirect=True)
+class TestSimpleConfig(ConfigFixtures):
+    def test_simple_load_vars(self, config):
+        assert list(config.vars().keys()) == ['foo' ,'bar']
+        assert config.var('bar') == 'hello, world'
+
+    def test_simple_load_params(self, config):
+        assert list(config.params().keys()) == ['jane']
+        assert config.param('jane') == 'Jane said "hello there"'
