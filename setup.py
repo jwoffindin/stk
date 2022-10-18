25fad232216203db99bcd6c983f30816cd74f7ab
John Woffindin <j.woffindin@gmail.com>
2022-10-18 15:10:29 +1300

    release 0.4.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     description="Opinionated CloudFormation deployments",
     license="MIT",
     author="John Woffindin <j.woffindin@gmail.com>",
-    version="0.4.0",
+    version="0.4.1",
     url="https://github.com/jwoffindin/stk",
     install_requires=["setuptools", "click", "boto3", "pyyaml", "jinja2", "rich", "cfn-flip", "gitpython", "git-url-parse"],
     entry_points={
