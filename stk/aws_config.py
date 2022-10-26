18296de51a9f02f2c7eed944103aaa26b9d6292c
John Woffindin <j.woffindin@gmail.com>
2022-10-27 12:32:13 +1300

    feat: add support for assume role / assume web role

--- stk/aws_config.py
+++ stk/aws_config.py
@@ -12,6 +12,11 @@ class AwsSettings:
     cfn_bucket: str
     account_id: str = None
     profile: str = None
+    role_arn: str = None
+    web_identity_token: str = None
+
+    def __post_init__(self):
+        self.__session = None
 
     def client(self, service):
         session = self._session()
@@ -31,19 +36,33 @@ class AwsSettings:
         return self.account_id
 
     def _session(self):
-        if self.profile:
-            session = boto3.Session(profile_name=str(self.profile))
-        else:
-            session = boto3.Session()
+        if not self.__session:
+            if self.role_arn and self.role_arn != "None":
+                if self.web_identity_token and self.web_identity_token != "None":
+                    short_token = self.web_identity_token[:8] + '\u2026' * (len(self.web_identity_token) > 8)
+                    log.info(f"assuming role {self.role_arn} using web token {short_token}")
+                    role = boto3.client('sts').assume_role_with_web_identity(RoleArn=self.role_arn, RoleSessionName='stk-web-session-role', WebIdentityToken=self.web_identity_token)
+                else:
+                    log.info(f"assuming role {self.role_arn}")
+                    role = boto3.client('sts').assume_role(RoleArn=self.role_arn, RoleSessionName="stk-session-role")
 
-        if not hasattr(self, "_checked_account"):
-            sts = session.client("sts")
-            account_id = sts.get_caller_identity()["Account"]
-            if self.account_id:
-                if str(account_id) != str(self.account_id):
-                    raise Exception(f"Incorrect AWS Account - expected {self.account_id}, but appear to be using {account_id} ")
+                credentials = role['Credentials']
+                session = boto3.Session(credentials['AccessKeyId'], credentials['SecretAccessKey'], credentials['SessionToken'])
+            elif self.profile:
+                session = boto3.Session(profile_name=str(self.profile))
+            else:
+                session = boto3.Session()
 
-            self._checked_account = True
-            self.account_id = account_id
+            if not hasattr(self, "_checked_account"):
+                sts = session.client("sts")
+                account_id = sts.get_caller_identity()["Account"]
+                if self.account_id:
+                    if str(account_id) != str(self.account_id):
+                        raise Exception(f"Incorrect AWS Account - expected {self.account_id}, but appear to be using {account_id} ")
 
-        return session
+                self._checked_account = True
+                self.account_id = account_id
+
+            self.__session = session
+
+        return self.__session
