6dabf6ddeea46214b097836f47c1cdc214eecc25
John Woffindin <j.woffindin@gmail.com>
2022-11-22 14:32:59 +1300

    fix: config add to support multiple repos
    Why:
    
    * If project is using repo A for templates, you can't add a template
      from B that refs A.
    
    This change addresses the need by:
    
    * Supporting "searching" for templates [B, then A]

--- stk/config_cmd/add_template.py
+++ stk/config_cmd/add_template.py
@@ -1,11 +1,29 @@
+from dataclasses import dataclass
+
 import os
 import pathlib
 import sys
+from typing import Any, Dict
 import yaml
 
 from .. import clog
 from ..provider import GitProvider
 
+
+@dataclass
+class RemoteFile:
+    """
+    Tuple of <content,provider>
+    """
+    content: str
+    provider: GitProvider
+
+    def template_config(self) -> dict:
+        """
+        returns config for template location that can be put into a config file; this exists
+        due to inconsistent naming between internal fields and user config. Needs to be fixed.
+        """
+        return { "root": self.provider.root or "/", "version": self.provider.git_ref, "repo": self.provider.git_url }
 
 class AddTemplateCmd:
     """
@@ -19,35 +37,31 @@ class AddTemplateCmd:
         #
         self.config_dir = config_dir
 
-        # If no repo is provided, use the default configuration from includes/common.yml
-        if not repo_url:
-            default = self._common("template")
-            if default:
-                self.repo_url = default["repo"]
-                self.provider = GitProvider(
-                    "templates",
+        self.providers = []
+
+        default = self._common("template")
+        self.providers.append(
+            GitProvider(
+                    "upstream",
                     git_url=default["repo"],
                     root=default["root"],
                     git_ref=default["version"]
-                )
-            else:
-                print(
-                    """unable to determine default template repository.
+            )
+        )
 
-                    Please pass --repo with git url of template repository
-                    """
+        if repo_url:
+            self.providers.insert(
+                0,
+                GitProvider(
+                    name="templates",
+                    git_url=repo_url,
+                    root="/",
+                    git_ref="main"
                 )
-                sys.exit(-1)
-        else:
-            self.repo_url = repo_url
-            self.provider = GitProvider(
-                name="templates",
-                git_url=repo_url,
-                root="/",
-                git_ref="main"
             )
 
-        clog(f"Using {self.provider.git_url} for remote template source")
+        provider_urls = [provider.git_url for provider in self.providers]
+        clog(f"Using { ', '.join(provider_urls) } for remote template source")
 
     def add(self, name: str, follow_refs: bool = True, inline: bool = False, local_template_dir: str = "templates") -> None:
         """
@@ -56,29 +70,41 @@ class AddTemplateCmd:
         follow_refs: if true, will attempt to add referenced stacks (if they don't exist)
         """
         template_filename = name + ".yaml"
-        if not self.is_remote_file(template_filename):
-            clog(
-                f"{template_filename} is not a file, or does not exist in remote repository")
-            sys.exit(-1)
 
-        # Info file in template repository has leaving prefix
-        info_filename = "_" + template_filename
-        if not self.is_remote_file(info_filename):
-            clog(
-                f"{info_filename} does not exist in remote repository - guessing defaults")
-            metadata = {"config": None}
-        else:
-            metadata = dict(yaml.safe_load(self.remote_content(info_filename)))
+        # Load remote template
+        try:
+            template = self.remote_content(template_filename)
+        except FileNotFoundError:
+            # Refs use underscore (since yaml doesn't like hyphens in keys), but have been using hyphens
+            # in stack names. Bit of a hack here :-/
+            kebab_name = name.replace('_', '-')
+            try:
+                self.add(kebab_name, follow_refs=follow_refs, inline=inline, local_template_dir=local_template_dir)
+                return
+            except:
+                clog(f"{template_filename}, nor {kebab_name} is not a file, or does not exist in remote repository")
+                raise
 
-        if metadata["config"] is None:
-            metadata["config"] = {}
+        # Load meta-data associated with remote template
+        #  file in template repository with _ prefix
+        metadata_filename = "_" + template_filename
 
-        # generate configuration file first (to avoid potential looks with auto-add)
+        metadata: Dict[str, Any] = {"config": {}}
+        try:
+            # Try to find metadata - only in the same provider, don't search self.providers
+            template_metadata = self.remote_content(metadata_filename, provider=template.provider)
+            metadata = dict(yaml.safe_load(template_metadata.content))
+            # generate configuration file first (to avoid potential looks with auto-add)
+            if metadata["config"] is None:
+                metadata["config"] = {}
+        except FileNotFoundError:
+            clog(f"{metadata_filename} does not exist in remote repository - guessing defaults")
 
+        # Build config object for this stack
         config = {
             "includes": ["common"],
             "environments": {x: None for x in self.core_environments()},
-            **metadata.get("config"),
+            **metadata.get("config"),  # type: ignore
         }
 
         if inline:
@@ -87,36 +113,34 @@ class AddTemplateCmd:
             # get started
             clog(f"Writing local template to {os.path.join(local_template_dir, template_filename)}")
             config["template"] = { "root": local_template_dir, "version": None, "repo": None }
-            template_content = self.remote_content(template_filename)
-            self.write_local_file(local_template_dir, template_filename,content=template_content)
+            self.write_local_file(local_template_dir, template_filename, content=template.content)
         else:
-            # Use remote template
-            template = {"root": "/", "version": "main", "repo": self.repo_url}
-            if template != self._common()["template"]:
-                config["template"] = template
+            # If template comes from repo that is not the default, override it in the config
+            if template.template_config() != self._common()["template"]:
+                config["template"] = template.template_config()
 
+        # Wrote configuration file
         clog(f"Writing config file {name}.yml")
-
         content = [f"# Starting configuration for deploying stack {name}\n"]
         if "description" in metadata and metadata["description"]:
-            content += ["# " + line for line in metadata["description"].splitlines(keepends=True)]
-        content += [yaml.safe_dump(config, default_flow_style=False, explicit_start=True, sort_keys=False)]
+            description = ["# " + line for line in metadata["description"].splitlines(keepends=True)]
+            content += description  # type: ignore
+
+        content += [
+            yaml.safe_dump(config, default_flow_style=False, explicit_start=True, sort_keys=False)
+        ]
 
         self.write_local_file(name + ".yml", content="".join(content))
 
         # try to resolve referenced stacks. E.g. if "ec2" stack depends on "vpc", then we
         # automatically do an "add" for "vpc" stack.
         if follow_refs and "refs" in metadata["config"]:
-            for ref in metadata["refs"].keys():
+            for ref in metadata["config"]["refs"].keys():
                 if self.is_local_file(ref + ".yml") or self.is_local_file(ref + ".yaml"):
                     clog(f"skipping {ref} - config exists already")
-                    continue
+                else:
+                    self.add(ref, local_template_dir=local_template_dir)
 
-    def is_remote_file(self, *p) -> bool:
-        """
-        Returns true if path exists in remote repository
-        """
-        return self.provider.is_file(*p)
 
     def is_local_file(self, *p) -> bool:
         """
@@ -125,11 +149,21 @@ class AddTemplateCmd:
         local_file = os.path.join(self.config_dir, *p)
         return os.path.isfile(local_file)
 
-    def remote_content(self, *p) -> str:
+    def remote_content(self, *p, provider=None) -> RemoteFile:
         """
         Returns content of remote file
         """
-        return str(self.provider.content(*p), encoding="utf-8")
+        if provider:
+            if provider.is_file(*p):
+                content = str(provider.content(*p), encoding="utf-8")
+                return RemoteFile(content, provider)
+        else:
+            for provider in self.providers:
+                if provider.is_file(*p):
+                    content = str(provider.content(*p), encoding="utf-8")
+                    return RemoteFile(content, provider)
+
+        raise FileNotFoundError(f"unable to find {os.path.join(*p)} in any remotes")
 
     def local_content(self, *p) -> str:
         """
