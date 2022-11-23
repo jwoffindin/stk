05d02733fe422cf13c1151a05b9d681510d64e9e
John Woffindin <j.woffindin@gmail.com>
2022-11-24 11:16:09 +1300

    Release 0.8.6

--- stk/__init__.py
+++ stk/__init__.py
@@ -7,7 +7,7 @@ import yaml
 from os import environ
 from rich.console import Console
 
-VERSION = "0.8.5"
+VERSION = "0.8.6"
 
 # Create application logger (for when things go wrong)
 log = logging.getLogger('cfn')
